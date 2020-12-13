dcbae766a8e1f545e7a9272700a729cf87ad1be1
Kristina Frederick <kristinafrederick@icloud.com>
2020-12-13 02:39:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  another_hash = nil
-  key_for_min_value.inject {|name, number|}
+  #another_hash = nil
+  #key_for_min_value.inject {|name, number|}
+  def key_for_min_value(hash)
+  hash.inject { |m,a| m[1] > a[1] ? a : m }[0] unless hash.empty?
+end
 end
