4980c71f0d965501f25b14c8c0d18385cc509ef2
Kristina Frederick <kristinafrederick@icloud.com>
2020-12-13 02:40:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,5 @@
 def key_for_min_value(name_hash)
   #another_hash = nil
   #key_for_min_value.inject {|name, number|}
-  def key_for_min_value(hash)
   hash.inject { |m,a| m[1] > a[1] ? a : m }[0] unless hash.empty?
-end
 end
