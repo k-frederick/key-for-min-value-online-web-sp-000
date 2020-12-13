d68e3b5c9bb249beddc1873856994289419a837a
Kristina Frederick <kristinafrederick@icloud.com>
2020-12-13 02:39:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  another_hash = nil
+  key_for_min_value.inject {|name, number|}
 end
